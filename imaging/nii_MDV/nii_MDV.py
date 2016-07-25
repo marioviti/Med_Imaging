bc94cbb03d19c86c3b154830267a493a175d9077
vitimario <vitimario2@gmail.com>
2016-07-25 16:53:21 +0200

    Patches
    done with 3D stuff. there was a bug but it has been fixed.

--- imaging/nii_MDV/nii_MDV.py
+++ imaging/nii_MDV/nii_MDV.py
@@ -10,43 +10,7 @@ import bisect
 import numpy as np
 import nii_maths
 import nii_utils as utils
-
-class Compressor:
-
-    def __init__(self, sampler, patternBank):
-        assert isinstance(mxmtr,Sampler), "must pass a Sampler."
-        self.__sampler = sampler
-        self.__patternbank = patternBank
-
-    def compress3D(self, image, verbose=False):
-        if not len(image) == 3:
-            raise ValueError("Must provide 3D image")
-        patchside = self.__sampler.patchside
-        bitDepth = self.__sampler.bitDepth
-        thrss = self.__sampler.bitDepth
-        if verbose:
-            print('start')
-        x, y, z = image.shape
-        imageCopy = np.reshape(nii_maths.thressholding(image.ravel(), self.__thrss),(x,y,z))
-        blankimageCopy = np.zeros(image.shape)
-        i=j=k=0
-        for i in range(0, x - self.__patchside + 1):
-            for j in range(0, y - self.__patchside + 1):
-                for k in range(0, z - self.__patchside + 1):
-                    key = nii_maths.patternKey(self.__bitDepth,
-                        imageCopy[i:i + self.__patchside, j:j + self.__patchside, k:k + self.__patchside] )
-                    if key in self.__patternbank:
-                        blankimageCopy[i:i + self.__patchside, j:j + self.__patchside, k:k + self.__patchside] = imageCopy[i:i + self.__patchside, j:j + self.__patchside, k:k + self.__patchside]
-            if verbose:
-                print ("\rprogress: ", int((i/float(x - self.__patchside))*100), "%" , end="")
-        if verbose:
-            print ('\nended')
-        return blankimageCopy
-
-    @property
-    def sampler(self):
-        return self.__sampler
-
+import copy
 
 class Maximizator:
 
@@ -61,7 +25,8 @@ class Maximizator:
 
     def maxim(self, bh, W=None, N=None):
         """
-        apply a feature reduction filter based on a entropy maximization with linear constratint of W weighet frequency, and N spatial constratint
+        Applies a feature reduction filter based on a entropy maximization
+        with linear constratint of W weighet frequency, and N spatial constratint
         """
         if not W == None:
             self.__W = W
@@ -70,8 +35,8 @@ class Maximizator:
         patternBank = {}
         assert isinstance(bh,nii_maths.binnedHistogram), "must pass a BinnedHistogram."
         offset = bh.precision
-        X = bh.X
-        Y = bh.Y
+        X = copy.deepcopy(bh.X)
+        Y = copy.deepcopy(bh.Y)
         exL, exR = (X[0] , X[len(X)-1])
         Il = Ir = cusp = math.log(self.__W/float(self.__N))
         selL = selR = count = 0
@@ -85,16 +50,83 @@ class Maximizator:
                 i += 1
             del X[selL:i+1]
             del Y[selL:i+1]
-        #self.__patternbank = nii_maths.binnedHistogram(bh.precision,bh.fun,patternBank)
+        del X
+        del Y
         self.__patternbank = patternBank
         return self.__patternbank
 
-class Sampler:
+class Compressor:
 
+    def __init__(self, sampler, patternBank):
+        assert isinstance(sampler,Sampler), "must pass a Sampler."
+        cond0 = all(isinstance(n, int) for n in patternBank)
+        cond1 = all(isinstance(n, long) for n in patternBank)
+        assert cond0 or cond1, "patternBank must be <list> of <int> or <long>"
+        self.__sampler = sampler
+        self.__patternbank = patternBank
+
+    @property
+    def sampler(self):
+        return self.__sampler
+
+    def compress3D(self, image, encode_extremes=False, verbose=False, shape=None):
+        if not len(image.shape) == 3:
+            raise ValueError("Must provide 3D image")
+        if verbose:
+            print('start')
+        patchside = self.__sampler.patchside
+        bitDepth = self.__sampler.bitDepth
+        thrss = self.__sampler.thrss
+        patternbank = self.__patternbank
+        shape_ = image.shape
+        if not shape == None:
+            shape_ = shape
+        x,y,z = shape_
+        if x < patchside or y < patchside or z <patchside:
+            raise ValueError("Must provide shape with at least the same size of patchside**3")
+        imageCopy = np.reshape(nii_maths.thressholding(image.ravel(), thrss),(x,y,z))
+        ########################################################################
+        if encode_extremes:
+            for i in range(x):
+                for j in range(y):
+                    for k in range(z):
+                        if imageCopy[i,j,k] == len(thrss):
+                            imageCopy[i,j,k]=0
+            if 2**bitDepth == len(thrss):
+                bitDepth -= 1
+        ########################################################################
+        blankimageCopy = np.zeros(shape_)
+        for i in range(x - patchside + 1):
+            for j in range(y - patchside + 1):
+                for k in range(z - patchside + 1):
+                    key = nii_maths.patternKey(bitDepth, imageCopy[i:i + patchside, j:j + patchside, k:k + patchside])
+                    if key in patternbank:
+                        blankimageCopy[i:i + patchside, j:j + patchside, k:k + patchside] = imageCopy[i:i + patchside, j:j + patchside, k:k + patchside]
+            if verbose:
+                print ("\rprogress: ", int(((i+1)/float(x-patchside+1))*100), "%" , end="")
+        if verbose:
+            print ('\nended')
+        return blankimageCopy
+
+class Sampler:
+    """
+    Sampling is a 2 parts process:
+    First data is downsampled according to thresholds,
+    there are 2 possible way to down sample data.
+        1 - thresholds are applied straightforward.
+        an example can clarify:
+        dwnsampl [0.9, 2.2, 5.1] using [1,2,3] = [0,2,3]
+        2 - thresholds will encode extremes as zeroes:
+        an example can clarify:
+        dwnsampl [0.9, 2.2, 5.1] using [1,2,3] = [0,2,0]
+        this method follows an entropy-like shape (nothing and everything are equally informative).
+    Second each subspace [0-patchside]**n (n is either 2 or 3) samples the [0-x] x [0-y] x [0-z] downsampled space and it's ecoded in
+    a single natural number.
+    """
     def __init__(self, patchside, thrss):
         self.__patchside = patchside
         self.__thrss = thrss
-        self.__bitDepth = int(math.log(len(self.__thrss)+1,2))
+        self.__bitDepth =int(math.ceil(math.log(len(self.__thrss)+1,2)))
 
     @property
     def patchside(self):
@@ -109,6 +141,10 @@ class Sampler:
         return self.__bitDepth
 
     def sample3Din2Dslices(self, image, direction=0, verbose=False):
+        """
+            Sample 3D array of data as 2D slices, direction will tell the axis
+            orthogonal to the 2D patches planes (independet).
+        """
         x, y, z = image.shape
         sample = curr_sample = {}
         if direction == 0:
@@ -131,56 +167,66 @@ class Sampler:
         """
         if not len(image.shape) == 2:
             raise ValueError("must provide 2D images")
-        if not int(math.log(len(self.__thrss)+1,2)) == self.__bitDepth :
+        if not math.ceil(math.log(len(self.__thrss)+1,2)) == self.__bitDepth:
             raise ValueError("incompatible thresholds and bitDepth")
-        x, y = image.shape
-        imageCopy = np.reshape(nii_maths.thressholding(image.ravel(), self.__thrss),(x,y))
-        i=j=0
-        freqHistogram = {}
         if verbose:
             print('start')
-        for i in range(0, x - self.__patchside + 1):
-            for j in range(0, y - self.__patchside + 1):
-                key = nii_maths.patternKey(self.__bitDepth,
-                    imageCopy[i:i + self.__patchside, j:j + self.__patchside] )
+        patchside = self.__patchside
+        thrss = self.__thrss
+        bitDepth = self.__bitDepth
+        x,y = image.shape
+        imageCopy = np.reshape(nii_maths.thressholding(image.ravel(), thrss),(x,y))
+        freqHistogram = {}
+        for i in range(x - patchside + 1):
+            for j in range(y - patchside + 1):
+                key = nii_maths.patternKey(bitDepth, imageCopy[i:i + patchside, j:j + patchside] )
                 if key in freqHistogram:
                     freqHistogram[key] += 1
                 else:
                     freqHistogram.update({key: 1})
             if verbose:
-                print ("\rprogress: ", int((i/float(x - self.__patchside))*100), "%" , end="")
-
+                print ("\rprogress: ", int(((i+1)/float(x-patchside+1))*100), "%" , end="")
         ret = { k : float(v) for k,v in freqHistogram.items() }
         if verbose:
             print ('\nended')
         return ret
 
-    def sample3D(self, image, verbose=False):
+    def sample3D(self, image, encode_extremes=False, verbose=False):
         """
             Sample 3D array of data
         """
         if not len(image.shape) == 3:
             raise ValueError("must provide 3D images")
-        if not int(math.log(len(self.__thrss)+1,2)) == self.__bitDepth :
+        if not math.ceil(math.log(len(self.__thrss)+1,2)) == self.__bitDepth:
             raise ValueError("incompatible thresholds and bitDepth")
-        x, y, z = image.shape
-        imageCopy = np.reshape(nii_maths.thressholding(image.ravel(), self.__thrss),(x,y,z))
-        i=j=k=0
-        freqHistogram = {}
         if verbose:
             print('start')
-        for i in range(0, x - self.__patchside + 1):
-            for j in range(0, y - self.__patchside + 1):
-                for k in range(0, z - self.__patchside + 1):
-                    key = nii_maths.patternKey(self.__bitDepth,
-                        imageCopy[i:i + self.__patchside, j:j + self.__patchside, k:k + self.__patchside] )
+        patchside = self.__patchside
+        thrss = self.__thrss
+        bitDepth = self.__bitDepth
+        x, y, z = image.shape
+        imageCopy = np.reshape(nii_maths.thressholding(image.ravel(), thrss),(x,y,z))
+        ########################################################################
+        if encode_extremes:
+            for i in range(x):
+                for j in range(y):
+                    for k in range(z):
+                        if imageCopy[i,j,k] == len(thrss):
+                            imageCopy[i,j,k]=0
+            if 2**bitDepth == len(thrss):
+                bitDepth -= 1
+        ########################################################################
+        freqHistogram = {}
+        for i in range(x - patchside + 1):
+            for j in range(y - patchside + 1):
+                for k in range(z - patchside + 1):
+                    key = nii_maths.patternKey(bitDepth, imageCopy[i:i + patchside, j:j + patchside, k:k + patchside] )
                     if key in freqHistogram:
                         freqHistogram[key] += 1
                     else:
                         freqHistogram.update({key: 1})
             if verbose:
-                print ("\rprogress: ", int((i/float(x - self.__patchside))*100), "%" , end="")
-
+                print ("\rprogress: ", int(((i+1)/float(x-patchside+1))*100), "%" , end="")
         ret = { k : float(v) for k,v in freqHistogram.items() }
         if verbose:
             print ('\nended')
